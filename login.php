4d33252e4cb08d7b751dbf7e22e0ec2adc2b3303
Guyinthecorner4 <93350113+Guyinthecorner4@users.noreply.github.com>
2024-02-27 15:01:01 -0500

    Update login.php
    Just added some comments

--- login.php
+++ login.php
@@ -4,9 +4,9 @@
 <?php
 $serverName = "localhost";
 $userName = "root";
-$password = "password";
-$dbName = "collegesystem";
-$serverName = ini_get("mysqli.default_host");
+$password = "password";// should be no specifed password! this is just saying the password is password, and is not the same.
+$dbName = "collegesystem";//she wants the databse to be  called DB2 not college systems
+$serverName = ini_get("mysqli.default_host");//why is this here? you are defining server name twice?
 echo $serverName;
 
     $conn = mysqli_connect($dbName);
