365d198efe2cbfb0db8f59ecad55a72f24e705ad
RohanG21 <96847964+RohanG21@users.noreply.github.com>
2024-02-27 14:53:37 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,23 @@
+//chronic moronic -- fire name for a food truck / dispensary
+ //big body 
+
+<?php
+$serverName = "localhost";
+$userName = "root";
+$password = "password";
+$dbName = "collegesystem";
+$serverName = ini_get("mysqli.default_host");
+echo $serverName;
+
+    $conn = mysqli_connect($dbName);
+
+    if($conn) { 
+        echo "sql database successfully connected to";
+    }
+    else {
+        die("failed to connect to sql database" .mysqli_connect_error());
+    }
+?>
+
+
 
